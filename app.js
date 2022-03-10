597c94c000457ca17c399de0219fd58440d5d4d1
Jucimeire Silva <jucimeiresilva@hotmail.com>
2022-03-10 19:50:32 -0300

    html resolvido, falta o css

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+
+
+const app = express();
+
+app.use(express.static('public'))
+
+app.get('/home', (request, response, next) => {
+  response.sendFile(`${__dirname}/views/home.html`)
+});
+
+app.get('/about', (request, response, next) => {
+  response.sendFile(`${__dirname}/views/about.html`)
+});
+
+app.get('/work', (request, response, next) => {
+  response.sendFile(`${__dirname}/views/work.html`)
+});
+
+
+app.listen(3000, () => console.log('server started'))
